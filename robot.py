8969a1c01d2198d7b41c65617ccbae6b0e130d47
Vincent Fiszbin <vincent.fiszbin@etu.sorbonne-universite.fr>
2022-01-20 11:11:12 +0100

    Ajoute les prototypes d'environnement et de robot
    Les attributs d'environnement sont : sa hauteur, largeur, une liste des objets contenus dans l'environnement
    Une représentation graphique peut être générée en utilisant la méthode Show2D()
    
    Les attributs du robot sont ses coordonnées x et y, ainsi au'une direction
    
    La documentation fournie sur les calculs de direction explique plus en détails l'utilisation de cet attribut

---
+++ robot.py
@@ -0,0 +1,11 @@
+class robot:
+    def __init__(self, xpos, ypos):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.dir = 90
+
+    def changeDir(self, dir):
+        if dir >= 0 and dir <= 360:
+            self.dir = dir
+        else:
+            print("La direction doit être comprise entre 0 et 360 degrés")
---
+++ test.py
@@ -0,0 +1,9 @@
+from environment import environment
+from robot import robot
+
+env = environment(400,300)
+rob = robot(200,140)
+env.addObject(rob)
+print(env.objects)
+rob.changeDir(135)
+env.show2D()
