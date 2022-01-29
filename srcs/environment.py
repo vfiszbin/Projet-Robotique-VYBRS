ec555d5920ed0b11d2a73bc7fc104a8398d7eba8
Vincent Fiszbin <vincent.fiszbin@etu.sorbonne-universite.fr>
2022-01-29 09:24:20 +0100

    Vérifie que les coordonnées d'un robot sont compatibles avec les dimensions de l'environnement avant d'y ajouter l'objet robot

--- srcs/environment.py
+++ srcs/environment.py
@@ -1,3 +1,6 @@
+from turtle import width
+from robot import robot
+
 class environment:
     def __init__(self, width, height):
         self.height = height
@@ -5,7 +8,15 @@ class environment:
         self.objects = [] #liste des objets présents dans l'environnement
 
     def addObject(self, obj):
-        self.objects.append(obj)
+        #Vérifie que l'objet robot a des coordonnées compatibles avec cet environnement
+        if isinstance(obj, robot):
+            x = obj.positionX
+            y = obj.positionY
+            if (x < 0 or x > self.width or y < 0 or y > self.height):
+                print(f"Coordonnées ({x},{y}) du robot incompatibles avec les dimensions (largeur={self.width},hauteur={self.height}) de l'environnement")
+            else:
+                self.objects.append(obj)
 
     def removeObject(self, obj):
         self.objects.remove(obj)
+    
