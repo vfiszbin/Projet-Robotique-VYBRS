a7e864f04a8dcd88bc725171e0047f2ed2b10d38
Vincent Fiszbin <vincent.fiszbin@etu.sorbonne-universite.fr>
2022-01-30 13:17:31 +0100

    Permet l'actualisation en temps réel de la représentation graphique
    Ajoute updateSimulation() qui s'execute dans un thread secondaire (si l'on souhaite afficher la représentation graphique)
    pendant que le l'affichage graphique occupe le main thread.
    La représentation graphique s'actualise toutes les x ms en appelant sa fonction update()

--- srcs/test.py
+++ srcs/test.py
@@ -3,27 +3,24 @@ from robot import robot
 from obstacle import Obstacle
 from viewer import show2D
 
+
 #Création de l'environnement
 env = environment(400,300)
 
 #Tests robot
 rob = robot(300,100)
 env.addObject(rob)
-print(env.objects)
+#print(env.objects)
 rob.changeDir(45)
 
 #Tests obstacle
 obs = Obstacle(10,50,100,30)
 env.addObject(obs)
-print(env.objects)
+#print(env.objects)
 obs2 = Obstacle(250,180,30,100)
 env.addObject(obs2)
-print(env.objects)
+#print(env.objects)
 
-#Affiche la représentation 2D de l'environnement
-show2D(env)
-
-# changer la direction du robot 
-rob.deplacerPositionRobotAvant(15,-7,45,20)
-rob.deplacerPositionRobotDerriere(-30,20,-160,-30)
+#Lance un thread secondaire qui execute updateSimulation et démarre l'affichage graphique
+show2D(env, rob)
 
--- srcs/viewer.py
+++ srcs/viewer.py
@@ -2,45 +2,74 @@ from tkinter import Tk, Canvas
 from math import cos, sin, pi
 from robot import robot
 from obstacle import Obstacle
+from updateSimulation import updateSimulation
 
-def show2D(environment):
-	window = Tk()
-	window.title("Environnement")
+from time import sleep
+import threading
 
-	canvas = Canvas(window, height=environment.height, width=environment.width)
-	canvas.pack()
 
-	for obj in environment.objects: #représentation visuelle de chaque objet ajouté à l'environnement
-		if isinstance(obj, robot):
-			#Création d'un cerle pour représenter la position du robot
-			r = 3 #rayon
-			x = obj.positionX
-			y = obj.positionY
-			x0 = x - r
-			y0 = y - r
-			x1 = x + r
-			y1 = y + r
-			canvas.create_oval(x0, y0, x1, y1, fill="red")
 
-			#Création d'une flèche représentant la direction du robot
-			#Voir Schéma explicatif des calculs de direction
-			x0 = x
-			y0 = y
-			len_arrow = 20
-			dir = obj.dir * pi / 180 #conversion des degrés en radians
-			dx = len_arrow * cos(dir)
-			dy = len_arrow * sin(dir)
-			x1 = x0 + dx
-			y1 = y0 - dy
-			canvas.create_line(x0, y0, x1, y1, arrow='last')
+def show2D(env, rob):
+	'''
+	L'affichage graphique avec tkinter requiert de lancer une mainloop() qui va bloquer l'execution de toute autre instructions (c'est équivalent à
+	une boucle while infinie). Pour continuer à executer d'autres instructions que l'affichage graphique on lance updateSimulation()
+	dans un thread secondaire qui s'execute en parallèle de l'affichage graphique.
+	'''
+	thread = threading.Thread(target=updateSimulation,args=(env,rob))
+	# thread.setDaemon(True) #pour que le thread meurt lorsque le main thread se termine
+	thread.start()
+	graphicalRepresentation(env)
 
-		elif isinstance(obj, Obstacle):
-			# (x0,y0) = sommet en haut à gauche du rectangle
-        	# (x1,y1) = sommet en bas à droite du rectangle 
-			x0 = obj.positionX
-			y0 = obj.positionY
-			x1 = x0 + obj.width
-			y1 = y0 + obj.height
-			canvas.create_rectangle(x0, y0, x1, y1, fill="grey")
 
-	window.mainloop()
+class graphicalRepresentation:
+	def __init__(self, environment):
+		self.window = Tk()
+		self.window.title("Environnement")
+		self.environment = environment
+		self.canvas = Canvas(self.window, height=environment.height, width=environment.width)
+		self.canvas.pack()
+
+		self.window.after(1000, self.update) #executera update() dans 1000 ms
+
+		self.window.mainloop() # bloque le main thread
+
+	def update(self):
+		'''
+		Une fois l'affichage graphique lancé en appelant mainloop(), l'execution de code est bloquée dans le main thread, 
+		on utilise donc after() qui va appeler cette fonction toutes les x ms pour mettre à jour l'affichage graphique
+		'''
+		for obj in self.environment.objects: #représentation visuelle de chaque objet ajouté à l'environnement
+			if isinstance(obj, robot):
+				#Création d'un cerle pour représenter la position du robot
+				r = 3 #rayon
+				x = obj.positionX
+				y = obj.positionY
+				x0 = x - r
+				y0 = y - r
+				x1 = x + r
+				y1 = y + r
+				self.canvas.create_oval(x0, y0, x1, y1, fill="red")
+
+				#Création d'une flèche représentant la direction du robot
+				#Voir Schéma explicatif des calculs de direction
+				x0 = x
+				y0 = y
+				len_arrow = 20
+				dir = obj.dir * pi / 180 #conversion des degrés en radians
+				dx = len_arrow * cos(dir)
+				dy = len_arrow * sin(dir)
+				x1 = x0 + dx
+				y1 = y0 - dy
+				self.canvas.create_line(x0, y0, x1, y1, arrow='last')
+
+			elif isinstance(obj, Obstacle):
+				# (x0,y0) = sommet en haut à gauche du rectangle
+				# (x1,y1) = sommet en bas à droite du rectangle 
+				x0 = obj.positionX
+				y0 = obj.positionY
+				x1 = x0 + obj.width
+				y1 = y0 + obj.height
+				self.canvas.create_rectangle(x0, y0, x1, y1, fill="grey")
+
+		self.window.after(1000, self.update) # update() se rappelle elle même toutes les 1000 ms 
+			
