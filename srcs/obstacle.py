7e944f614adbf0309dadc6b35b7a97ec329faf64
Rannn00 <achek.ranya94@gmail.com>
2022-01-30 16:37:07 +0100

    creation d'objet mur +tests des taches

--- srcs/obstacle.py
+++ srcs/obstacle.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 class Obstacle:
      def __init__(self,positionX,positionY,width,height):
          """  (positionX,positionY) : les coordonnes du point d'extremites en haut a gauche (pour reperer les obstacles)
@@ -6,3 +7,11 @@ class Obstacle:
          self.positionY=positionY
          self.width=width
          self.height=height
+
+#pour l'instant la classe fait des mur 90 et 0 deg uniquement
+class wall(Obstacle):
+     def __init__(self,positionX,positionY,length,dire):
+          if (dire == 90) or (dire==270):
+               super().__init__(positionX,positionY,5,length)
+          else :
+               super().__init__(positionX,positionY,length,5)
--- srcs/test.py
+++ srcs/test.py
@@ -1,16 +1,17 @@
+# -*- coding: UTF-8 -*-
 from environment import environment
 from robot import robot
-from obstacle import Obstacle
+from obstacle import Obstacle,wall
 from viewer import show2D
 
-#Création de l'environnement
-env = environment(400,300)
+#Creation de l'environnement
+env = environment(800,300)
 
 #Tests robot
-rob = robot(300,100)
+rob = robot(300,240)
 env.addObject(rob)
 print(env.objects)
-rob.changeDir(45)
+rob.changeDir(90)
 
 #Tests obstacle
 obs = Obstacle(10,50,100,30)
@@ -20,10 +21,21 @@ obs2 = Obstacle(250,180,30,100)
 env.addObject(obs2)
 print(env.objects)
 
-#Affiche la représentation 2D de l'environnement
+# Ajout d'un mur 
+mur=wall(0,0,300,90)
+mur2=wall(795,0,300,90)
+mur3=wall(0,0,800,0)
+mur4=wall(0,295,800,0)
+env.addObject(mur)
+env.addObject(mur2)
+env.addObject(mur3)
+env.addObject(mur4)
+
+#Affiche la representation 2D de l'environnement
 show2D(env)
 
 # changer la direction du robot 
 rob.deplacerPositionRobotAvant(15,-7,45,20)
 rob.deplacerPositionRobotDerriere(-30,20,-160,-30)
+
 
