15cfe86be5972c1b53a1358c83fdfa19501d6218
Vincent Fiszbin <vincent.fiszbin@etu.sorbonne-universite.fr>
2022-01-30 13:35:43 +0100

    Commentaire qui précise la raison pour laquelle on efface tous les objets de la représentation graphique

--- srcs/viewer.py
+++ srcs/viewer.py
@@ -35,7 +35,7 @@ class graphicalRepresentation:
 		Une fois l'affichage graphique lancé en appelant mainloop(), l'execution de code est bloquée dans le main thread, 
 		on utilise donc after() qui va appeler cette fonction toutes les x ms pour mettre à jour l'affichage graphique
 		'''
-		self.canvas.delete("all") # efface tous les objets déjà affichés
+		self.canvas.delete("all") # efface tous les objets déjà affichés, cela évite de créer des doubles
 		for obj in self.environment.objects: #représentation visuelle de chaque objet ajouté à l'environnement
 			if isinstance(obj, robot):
 				#Création d'un cerle pour représenter la position du robot
