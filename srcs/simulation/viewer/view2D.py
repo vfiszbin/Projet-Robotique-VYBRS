ac209e8a89a085b0737aa79e1ec3ff3b04b0767a
Vincent Fiszbin <vincent.fiszbin@etu.sorbonne-universite.fr>
2022-02-07 19:28:51 +0100

    Affichage du robot et des obstacles se fait dans des fonctions séparées de l'update d'affichage

--- srcs/simulation/viewer/view2D.py
+++ srcs/simulation/viewer/view2D.py
@@ -27,35 +27,41 @@ class View2D:
 		self.canvas.delete("all") # efface tous les objets déjà affichés, cela évite de créer des doubles
 		for obj in self.environment.objects: #représentation visuelle de chaque objet ajouté à l'environnement
 			if isinstance(obj, Robot):
-				#Création d'un cerle pour représenter la position du robot
-				r = 3 #rayon
-				x = obj.positionX
-				y = obj.positionY
-				x0 = x - r
-				y0 = y - r
-				x1 = x + r
-				y1 = y + r
-				self.canvas.create_oval(x0, y0, x1, y1, fill="red")
-
-				#Création d'une flèche représentant la direction du robot
-				#Voir Schéma explicatif des calculs de direction
-				x0 = x
-				y0 = y
-				len_arrow = 20
-				dir = obj.dir * pi / 180 #conversion des degrés en radians
-				dx = len_arrow * cos(dir)
-				dy = len_arrow * sin(dir)
-				x1 = x0 + dx
-				y1 = y0 - dy
-				self.canvas.create_line(x0, y0, x1, y1, arrow='last')
+				self.displayRobot(obj)
 
 			elif isinstance(obj, Obstacle):
-				# (x0,y0) = sommet en haut à gauche du rectangle
-				# (x1,y1) = sommet en bas à droite du rectangle
-				x0 = obj.positionX
-				y0 = obj.positionY
-				x1 = x0 + obj.width
-				y1 = y0 + obj.height
-				self.canvas.create_rectangle(x0, y0, x1, y1, fill="grey")
+				self.displayObstacle(obj)
 
 		self.window.after(1000, self.update) # update() se rappelle elle même toutes les 1000 ms
+
+	def displayRobot(self, rob):
+		#Création d'un cerle pour représenter la position du robot
+		r = 3 #rayon
+		x = rob.positionX
+		y = rob.positionY
+		x0 = x - r
+		y0 = y - r
+		x1 = x + r
+		y1 = y + r
+		self.canvas.create_oval(x0, y0, x1, y1, fill="red")
+
+		#Création d'une flèche représentant la direction du robot
+		#Voir Schéma explicatif des calculs de direction
+		x0 = x
+		y0 = y
+		len_arrow = 20
+		dir = rob.dir * pi / 180 #conversion des degrés en radians
+		dx = len_arrow * cos(dir)
+		dy = len_arrow * sin(dir)
+		x1 = x0 + dx
+		y1 = y0 - dy
+		self.canvas.create_line(x0, y0, x1, y1, arrow='last')
+
+	def displayObstacle(self, obs):
+		# (x0,y0) = sommet en haut à gauche du rectangle
+		# (x1,y1) = sommet en bas à droite du rectangle
+		x0 = obs.positionX
+		y0 = obs.positionY
+		x1 = x0 + obs.width
+		y1 = y0 + obs.height
+		self.canvas.create_rectangle(x0, y0, x1, y1, fill="grey")
