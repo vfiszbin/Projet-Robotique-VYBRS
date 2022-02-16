b43221f82b9cdd144a6567d207f38e0169e7f865
Rannn00 <achek.ranya94@gmail.com>
2022-02-16 11:12:14 +0100

    la classe strategie

--- srcs/simulation/controller/controller.py
+++ srcs/simulation/controller/controller.py
@@ -13,5 +13,58 @@ def TestStrategy(rob):
 	rob.changeSpeed(-20)
 	sleep(3)
 	rob.changeSpeed(0)
+class Strategy:
+	"""
+	classe qui Cree une nouvelle strategie
+	"""
+	def __init__(self):
+		self.begin=False #chaque strategie a un debut et une fin
+		self.end=False
+	
+	def start(self):
+		"""
+		methode qui commence une strategie
+		"""
+		self.begin=True
+	def stop(self):
+		"""
+		methode qui termine la strategie
+		"""
+		self.end=True
 
+class move(Strategy):
+	"""
+	class de strategie permet d'avancer le robot
+	"""
+	def __init__(self,rob,distance,speed):
+		super().__init__()
+		self.speed=speed
+		self.distance=distance
+		self.positionXB=rob.getPositionXRobot()
+		self.positionYB=rob.getPositionYRobot()
+		self.rob=rob
+	def start(self):
+		super().start()
+		self.rob.changeSpeed(self.speed)
+		self.rob.deplacerPositionRobotAvant(self.distance)
 
+	def stop(self):
+		if distance_covered(positionXB,positionYB,rob.getPositionXRobot(),rob.getPositionYRobot()) == self.distance :
+			super().stop()
+		else :
+			print("fail")
+		
+
+class turn(Strategy):
+	"""
+	class de strategie: permet de tourner le robot	et l'avancer	
+	"""
+	def __init__(self,rob,dir):
+		super().__init__()
+		self.dir=dir
+		self.rob=rob
+	def start(self):
+		super().start()
+		self.rob.changeDir(self.dir)
+	def stop(self):
+		self.end = (rob.getDir()==self.dir)
