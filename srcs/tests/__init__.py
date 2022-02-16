ab5503aa42ab1467c6d09d3e67207621a65de93f
vfiszbin <28711002@ppti-14-406-11.ufr-info-p6.jussieu.fr>
2022-02-16 11:07:35 +0100

    Retire import de MoveForward

--- srcs/tests/__init__.py
+++ srcs/tests/__init__.py
@@ -3,4 +3,3 @@ from simulation.modele.obstacle import Obstacle, Wall
 from simulation.modele.robot import Robot
 from simulation.viewer.view2D import View2D
 from simulation.modele.updateModele import UpdateModele
-from simulation.controller.controller import moveForward
