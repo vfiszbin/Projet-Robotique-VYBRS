1d6fe1985990b9af2017b83f814be8d3f133ceb9
Vincent Fiszbin <vincent.fiszbin@etu.sorbonne-universite.fr>
2022-02-26 20:45:06 +0100

    Complète turnStrategy() qui fait tourner le robot d'un certain angle à une certaine vitesse

--- srcs/simulation/controller/controller.py
+++ srcs/simulation/controller/controller.py
@@ -1,36 +1,45 @@
 from time import sleep
 
-UPDATE_TIME = 1 
+UPDATE_FREQUENCY = 0.1 #en secondes
 
 def TestStrategy(rob):
-	sleep(2)
-	rob.changeWheelMode(2)
-	rob.changeSpeed(100)
-	sleep(1)
-	rob.changeSpeed(0)
-	sleep(1)
-	rob.changeSpeed(-120)
-	sleep(1)
-	rob.changeSpeed(0)
-	sleep(1)
-	rob.changeSpeed(380)
-	sleep(1)
-	rob.changeSpeed(0)
 
-	rob.changeWheelMode(1)
-	rob.changeSpeed(360)
-	sleep(5)
-	rob.changeSpeed(0)
-	rob.changeWheelMode(2)
-	rob.changeSpeed(-260)
-	sleep(1)
-	rob.changeWheelMode(1)
-	rob.changeSpeed(360)
-	sleep(3)
-	rob.changeSpeed(0)
-	rob.changeSpeed(-180)
-	sleep(3)
-	rob.changeSpeed(0)
+	sleep(2)
+	strat = TurnStrategy(rob, -90, -40)
+	strat.start()
+	while not strat.stop():
+		print(rob.dir)
+		strat.step()
+		sleep(UPDATE_FREQUENCY)
+	print(rob.dir)
+
+	# rob.changeWheelMode(2)
+	# rob.changeSpeed(100)
+	# sleep(1)
+	# rob.changeSpeed(0)
+	# sleep(1)
+	# rob.changeSpeed(-120)
+	# sleep(1)
+	# rob.changeSpeed(0)
+	# sleep(1)
+	# rob.changeSpeed(380)
+	# sleep(1)
+	# rob.changeSpeed(0)
+
+	# rob.changeWheelMode(1)
+	# rob.changeSpeed(360)
+	# sleep(5)
+	# rob.changeSpeed(0)
+	# rob.changeWheelMode(2)
+	# rob.changeSpeed(-260)
+	# sleep(1)
+	# rob.changeWheelMode(1)
+	# rob.changeSpeed(360)
+	# sleep(3)
+	# rob.changeSpeed(0)
+	# rob.changeSpeed(-180)
+	# sleep(3)
+	# rob.changeSpeed(0)
 
 def launchStrategySeq(rob):
 	strat = StrategySeq()
@@ -53,7 +62,7 @@ class StrategySeq :
 		self.sequence.remove(strat)
 
 	def step(self):
-		if self.stop(): #toutes les strat de la seq ont été executés
+		if self.stop(): #toutes les strat de la seq ont été executées
 			return
 
 		if self.current_strat < 0 or self.sequence[self.current_strat].stop(): #démarrage de la prochaine strat
@@ -94,35 +103,32 @@ class move(Strategy):
 			print("fail")
 
 
-# class turn(Strategy):
-# 	"""
-# 	class de strategie: permet de tourner le robot	et l'avancer
-# 	"""
-# 	def __init__(self,rob,dir):
-# 		super().__init__()
-# 		self.dir=dir
-# 		self.rob=rob
-# 	def start(self):
-# 		self.begin=True
-# 		self.rob.changeDir(self.dir)
-# 	def stop(self):
-# 		if (rob.getDir()==self.dir) :
-# 			self.end = True
-# 		else :
-# 			print("fail")
+class TurnStrategy:
+	def __init__(self, rob, angle, speed):
+		#Vitesse et angle doivent avoir le même signe (aller dans la même direction)
+		self.rob = rob
+		self.speed = speed
+		self.angle_to_rotate = angle
+		self.angle_rotated_left_wheel = 0
+		self.angle_rotated_right_wheel = 0
 
+	def start(self):
+		self.angle_rotated_left_wheel = 0
+		self.angle_rotated_right_wheel = 0
+		self.rob.changeWheelMode(2) #passe les roues en mode tourner
 
-# class TurnStrategy:
-# 	def __init__(self, distance):
-# 		self.dist_to_cover = distance
+	def step(self):
+		self.rob.changeSpeed(0) #stop le robot à chaque début de step, nécessaire ?
+		#Récupère l'angle dont ont tournées les roues du robot depuis le début de la stratégie
+		self.angle_rotated_left_wheel = self.rob.angle_rotated_left_wheel
+		self.angle_rotated_right_wheel = self.rob.angle_rotated_right_wheel
+		if self.stop(): 
+			return
+		self.rob.changeSpeed(self.speed)
 
-# 	def start(self):
-# 		self.dist_covered = 0
-
-# 	def step(self):
-# 		self.dist_covered += 	
-# 		if self.stop(): return
-# 		self.avancer()
-
-# 	def stop(self):
-# 		return self.parcouru>self.distance
+	def stop(self):
+		#Vérifie si les roues ont tournées de l'angle demandé
+		if self.angle_to_rotate >= 0:
+			return self.angle_rotated_left_wheel <= -(self.angle_to_rotate) and self.angle_rotated_right_wheel >= self.angle_to_rotate
+		else:
+			return self.angle_rotated_left_wheel >= -(self.angle_to_rotate) and self.angle_rotated_right_wheel <= self.angle_to_rotate
