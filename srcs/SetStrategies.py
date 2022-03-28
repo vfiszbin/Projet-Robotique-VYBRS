4be5bd6970fe2bc7eb884dc16e4510cbe9019553
Vincent Fiszbin <vincent.fiszbin@etu.sorbonne-universite.fr>
2022-03-28 11:56:10 +0200

    Stratégie simple turn pour tester

--- srcs/SetStrategies.py
+++ srcs/SetStrategies.py
@@ -18,15 +18,20 @@ class SetStrategies :
 
 		#Prépare des séquences de stratégies que le controleur va executer
 		self.sequences = [] #la liste contenant les séquences de stratégies
-		seq1 = SquareStrategy(proxy,300,50)
-		self.sequences.append(seq1)
 		seq2=StrategySeq()
-		s1=moveForwardStrategy(proxy,200,50)
-		s2=TurnStrategy(proxy,-70,-80)
-		s3=moveBackwardStrategy(proxy, 250, 30)
-		s4=Navigate(proxy,250,200) # tester avec une plus grande distance pour voir comment se comporte le robot. 
-		seq2.addStrategy(s1)
+		s2=TurnStrategy(proxy,90,50)
 		seq2.addStrategy(s2)
-		seq2.addStrategy(s3)
-		seq2.addStrategy(s4)
 		self.sequences.append(seq2)
+
+		# seq1 = SquareStrategy(proxy,300,50)
+		# self.sequences.append(seq1)
+		# seq2=StrategySeq()
+		# s1=moveForwardStrategy(proxy,200,50)
+		# s2=TurnStrategy(proxy,-70,-80)
+		# s3=moveBackwardStrategy(proxy, 250, 30)
+		# s4=Navigate(proxy,250,200) # tester avec une plus grande distance pour voir comment se comporte le robot. 
+		# seq2.addStrategy(s1)
+		# seq2.addStrategy(s2)
+		# seq2.addStrategy(s3)
+		# seq2.addStrategy(s4)
+		# self.sequences.append(seq2)
